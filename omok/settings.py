988245ec4ebe9b3e7ffbfd092a9794072b96329b
Jeon Daeseong <jun65800@unist.ac.kr>
2019-08-03 22:02:01 +0900

    let server manage static files 2

--- omok/settings.py
+++ omok/settings.py
@@ -122,10 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-
 STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
